df975511ed0c5ab0fc458d7eb87767ed58be068b
sharmaharshitnone <sharmaharshitnone@gmail.com>
2026-01-13 11:45:55 +0530

    Refactor package installation commands for improved readability in install script

--- setup-recovery/install-packages.sh
+++ setup-recovery/install-packages.sh
@@ -18,30 +18,29 @@ sudo pacman -Sy --noconfirm &>/dev/null || log_warn "Pacman sync failed"
 
 # PACMAN PACKAGES (Official Repositories) - 111 packages
 log_info "Installing official packages via pacman (111 packages)..."
-sudo pacman -S --needed --noconfirm \
-  7zip act adw-gtk-theme alsa-utils android-tools android-udev \
-  base base-devel bat bc bemenu bemenu-wayland blueman bluez bluez-utils \
-  boost brightnessctl btop calcurse ccache cliphist cloudflared dkms dmenu \
-  dmidecode docker docker-buildx docker-compose dunst efibootmgr eza fd \
-  firefox flatpak foliate fzf gammastep gdb gimp git github-cli glow go \
-  gparted grim gvfs gvfs-mtp hyperfine i2c-tools i3-wm i3lock i3status-rust \
-  imagemagick intel-media-driver intel-ucode iwd jdk-openjdk jq kdenlive kitty \
-  lazydocker lazygit lib32-gamemode lib32-nvidia-utils lib32-vulkan-icd-loader \
-  libva-nvidia-driver linux linux-firmware linux-headers lldb luarocks ly lynx \
-  maim mako man-db mat2 materia-gtk-theme mediainfo mold mpc mpd mpd-mpris mpv \
-  neovim network-manager-applet networkmanager noto-fonts \
-  noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nsxiv nvidia-open-dkms nvidia-prime nvtop \
-  obs-studio opendoas openrgb pacman-contrib pandoc-cli papirus-icon-theme \
-  parallel pass pavucontrol picard picom clipmenu pipewire pipewire-alsa \
-  pipewire-pulse polkit-gnome python-i3ipc python-pipx python-setuptools qt6ct \
-  ranger rclone redshift reflector ripgrep rmpc rnote rtkit rust sbctl sheldon \
-  slurp smartmontools speech-dispatcher stow sway swaybg task \
-  telegram-desktop thermald thunar timew tmux ttf-dejavu ttf-jetbrains-mono-nerd \
-  ttf-liberation ttf-nerd-fonts-symbols-mono tumbler udiskie ufw unclutter \
-  usbutils valgrind virtualbox virtualbox-host-modules-arch vit vlc vulkan-intel \
-  waybar wget wireless_tools xclip xdg-utils xdotool xorg-xev xorg-xinit \
-  xorg-xinput xorg-xrandr xorg-xwininfo xss-lock xwallpaper yazi yt-dlp \
-  zathura zathura-cb zathura-pdf-poppler zeal zig zoxide zram-generator zsh
+sudo pacman -S --needed --noconfirm 7zip act adw-gtk-theme alsa-utils android-tools android-udev
+sudo pacman -S --needed --noconfirm base base-devel bat bc bemenu bemenu-wayland blueman bluez bluez-utils
+sudo pacman -S --needed --noconfirm boost brightnessctl btop calcurse ccache cliphist cloudflared dkms dmenu
+sudo pacman -S --needed --noconfirm dmidecode docker docker-buildx docker-compose dunst efibootmgr eza fd
+sudo pacman -S --needed --noconfirm firefox flatpak foliate fzf gammastep gdb gimp git github-cli glow go
+sudo pacman -S --needed --noconfirm gparted grim gvfs gvfs-mtp hyperfine i2c-tools i3-wm i3lock i3status-rust
+sudo pacman -S --needed --noconfirm imagemagick intel-media-driver intel-ucode iwd jdk-openjdk jq kdenlive kitty
+sudo pacman -S --needed --noconfirm lazydocker lazygit lib32-gamemode lib32-nvidia-utils lib32-vulkan-icd-loader
+sudo pacman -S --needed --noconfirm libva-nvidia-driver linux linux-firmware linux-headers lldb luarocks ly lynx
+sudo pacman -S --needed --noconfirm maim mako man-db mat2 materia-gtk-theme mediainfo mold mpc mpd mpd-mpris mpv
+sudo pacman -S --needed --noconfirm neovim network-manager-applet networkmanager noto-fonts
+sudo pacman -S --needed --noconfirm noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nsxiv nvidia-open-dkms nvidia-prime nvtop
+sudo pacman -S --needed --noconfirm obs-studio opendoas openrgb pacman-contrib pandoc-cli papirus-icon-theme
+sudo pacman -S --needed --noconfirm parallel pass pavucontrol picard picom clipmenu pipewire pipewire-alsa
+sudo pacman -S --needed --noconfirm pipewire-pulse polkit-gnome python-i3ipc python-pipx python-setuptools qt6ct
+sudo pacman -S --needed --noconfirm ranger rclone redshift reflector ripgrep rmpc rnote rtkit rust sbctl sheldon
+sudo pacman -S --needed --noconfirm slurp smartmontools speech-dispatcher stow sway swaybg task
+sudo pacman -S --needed --noconfirm telegram-desktop thermald thunar timew tmux ttf-dejavu ttf-jetbrains-mono-nerd
+sudo pacman -S --needed --noconfirm ttf-liberation ttf-nerd-fonts-symbols-mono tumbler udiskie ufw unclutter
+sudo pacman -S --needed --noconfirm usbutils valgrind virtualbox virtualbox-host-modules-arch vit vlc vulkan-intel
+sudo pacman -S --needed --noconfirm waybar wget wireless_tools xclip xdg-utils xdotool xorg-xev xorg-xinit
+sudo pacman -S --needed --noconfirm xorg-xinput xorg-xrandr xorg-xwininfo xss-lock xwallpaper yazi yt-dlp
+sudo pacman -S --needed --noconfirm zathura zathura-cb zathura-pdf-poppler zeal zig zoxide zram-generator zsh
 
 if [[ $? -eq 0 ]]; then
     log_info "✓ Pacman packages installed successfully"
